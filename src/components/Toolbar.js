11d7e28fb6a417d2ad19ddf11828a75a2b2f9982
Victor Garcia <victor.garcia@gig.com>
2020-11-13 08:02:59 +0100

    Store logic removed from Toolbar component and added to container

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -1,28 +1,33 @@
-import { observer } from "mobx-react";
 import React from "react";
-import store from "../stores/MainStore";
-
 
 function Toolbar(props) {
+
     const {
       addBoxToStore,
       removeAllBoxes,
       handleRemoveSelectedBoxes,
       getSelectedBoxesLabel,
       setColorToSelectedBoxes,
+      handleUndo,
+      handleRedo,
+      hasNoBoxes,
+      hasNoSelectedBoxes,
+      isNotUndoable,
+      isNotRedoable,
     } = props;
+
 
     return (
       <div className="toolbar">
         <button onClick={addBoxToStore}>Add Box</button>
-        <button onClick={removeAllBoxes} disabled={!store.boxes.length}>Remove All Boxes</button>
-        <button onClick={handleRemoveSelectedBoxes} disabled={!store.getSelectedBoxes().length}>Remove selected Box/es</button>
-        <button onClick={store.undo} disabled={!store.canUndo()}>Undo</button>
-        <button onClick={store.redo} disabled={!store.canRedo()}>Redo</button>
-        <input type="color" onChange={setColorToSelectedBoxes} disabled={!store.getSelectedBoxes().length} />
+        <button onClick={removeAllBoxes} disabled={hasNoBoxes}>Remove All Boxes</button>
+        <button onClick={handleRemoveSelectedBoxes} disabled={hasNoSelectedBoxes}>Remove selected Box/es</button>
+        <button onClick={handleUndo} disabled={isNotUndoable}>Undo</button>
+        <button onClick={handleRedo} disabled={isNotRedoable}>Redo</button>
+        <input type="color" onChange={setColorToSelectedBoxes} disabled={hasNoSelectedBoxes} />
         <span>{getSelectedBoxesLabel()}</span>
       </div>
     );
 }
 
-export default observer(Toolbar);
+export default Toolbar;
--- src/containers/Toolbar.container.js
+++ src/containers/Toolbar.container.js
@@ -1,35 +1,51 @@
+import { observer } from "mobx-react";
 import React, { Component } from "react";
 import Toolbar from "../components/Toolbar";
 import store from "../stores/MainStore";
 import { updateAxisOfAllBoxes } from "../utils/boxes/position.utils";
-import { bindContextToFunctionList } from "../utils/react/react";
 
 class ToolbarContainer extends Component {
-    constructor() {
-        super();
-
-        const bindThisToFunctions = bindContextToFunctionList(this);
-        bindThisToFunctions([ 'handleRemoveSelectedBoxes' ]);
-    }
-
-
+    
     async handleRemoveSelectedBoxes() {
         await store.removeSelectedBoxes();
         updateAxisOfAllBoxes();
     }
 
 
+    async handleUndo() {
+        await store.undo();
+        updateAxisOfAllBoxes();
+    }
+
+
+    async handleRedo() {
+        await store.redo();
+        updateAxisOfAllBoxes();
+    }
+
+
     render() {
+        const hasNoBoxes = !store.boxes.length;
+        const hasNoSelectedBoxes = !store.getSelectedBoxes().length;
+        const isNotUndoable = !store.canUndo();
+        const isNotRedoable = !store.canRedo();
+
         return (
-          <Toolbar
-              addBoxToStore={store.addBoxToStore}
-              removeAllBoxes={store.removeAllBoxes}
-              handleRemoveSelectedBoxes={this.handleRemoveSelectedBoxes}
-              getSelectedBoxesLabel={store.getSelectedBoxesLabel}
-              setColorToSelectedBoxes={store.setColorToSelectedBoxes}
-          />
+            <Toolbar
+                addBoxToStore={store.addBoxToStore}
+                removeAllBoxes={store.removeAllBoxes}
+                handleRemoveSelectedBoxes={this.handleRemoveSelectedBoxes}
+                getSelectedBoxesLabel={store.getSelectedBoxesLabel}
+                setColorToSelectedBoxes={store.setColorToSelectedBoxes}
+                handleUndo={this.handleUndo}
+                handleRedo={this.handleRedo}
+                hasNoBoxes={hasNoBoxes}
+                hasNoSelectedBoxes={hasNoSelectedBoxes}
+                isNotUndoable={isNotUndoable}
+                isNotRedoable={isNotRedoable}
+            />
         );
     }
 }
 
-export default ToolbarContainer;
+export default observer(ToolbarContainer);
